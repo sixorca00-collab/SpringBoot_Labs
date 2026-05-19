42210aae37f7835e04a3cf7f0ab64e320f2c4a7b
sixorca00-collab <sixorca00@gmail.com>
2026-05-19 12:02:02 -0500

    lab 2  finished

--- src/main/java/com/io/librotech/controller/LibroUIController.java
+++ src/main/java/com/io/librotech/controller/LibroUIController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller // Ojo: Aquí NO usamos @RestController, solo @Controller
-@RequestMapping("/admin/libros")
+@RequestMapping("/ui/libros")
 public class LibroUIController {
 
     @Autowired
@@ -27,5 +29,24 @@ public class LibroUIController {
 
         // 3. Le decimos a Spring que busque el archivo HTML llamado "lista" dentro de la carpeta "libros"
         return "libros/lista";
+    }
+
+    @GetMapping("/nuevo")
+    public String mostrarFormularioCreacion(Model model) {
+        // Enviamos una instancia vacía que el formulario de Thymeleaf va a llenar
+        model.addAttribute("libro", new Libro());
+        model.addAttribute("tituloPantalla", "Registrar Nuevo Libro");
+
+        return "libros/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarLibro(@ModelAttribute("libro") Libro libro) {
+        // 1. Guardamos el libro usando el método real de tu servicio
+        libroService.saveBook(libro);
+
+        // 2. Patrón PRG: Redirigimos a la lista de libros mediante un GET
+        // La palabra "redirect:" le dice al navegador que haga una nueva petición limpia
+        return "redirect:/ui/libros";
     }
 }
