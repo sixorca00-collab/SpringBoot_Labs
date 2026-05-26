cf18eb0e92d5d0b9b236bd425b8aa70b8996b4d4
Sixorca00-collab <sixorca00@gmail.com>
2026-05-26 08:14:53 -0500

    feat(LibroResumeDTO): generate DTO of books

---
+++ src/main/java/com/io/librotech/dto/LibroResumeDTO.java
@@ -0,0 +1,7 @@
+package com.io.librotech.dto;
+
+import java.time.LocalDate;
+
+public record LibroResumeDTO(Long id, String titulo, LocalDate fechaPublicacion, Double precio, String editorialNombre, String Pais) {
+
+}
